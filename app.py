d85b29faa4c25b4044725585cd461761bf0c2a92
jade0304 <stefanie.dn88@gmail.com>
2021-12-21 09:39:20 -0600

    writing tests

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def get_cupcake(cupcake_id):
     return jsonify(cupcake=cupcake.to_dict())
 
 
-@app.route("/api/cupcakes/<int:cupcake_id>")
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["PATCH"])
 def update_cupcake(cupcake_id):
     """Update a cupcake with the id"""
     
@@ -89,7 +89,7 @@ def update_cupcake(cupcake_id):
     return jsonify(cupcake=cupcake.to_dict())
 
 
-@app.route("/api/cupcakes/<int:cupcake_id>")
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["DELETE"])
 def delete_cupcake(cupcake_id):
     """Delete specific cupcake."""
 
